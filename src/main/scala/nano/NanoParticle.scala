07e051301a90f48cd6d64d30e15cb8bf969acad7
objektwerks <objektwerks@runbox.com>
2022-02-23 04:24:00 -0500

    added circuit

--- src/main/scala/nano/NanoParticle.scala
+++ src/main/scala/nano/NanoParticle.scala
@@ -7,3 +7,5 @@ final case class QuantumDot() extends NanoParticle
 final case class QuantumCell(quantumDots: Seq[QuantumDot]) extends NanoParticle
 
 final case class Layer(quantumCells: Seq[QuantumCell]) extends NanoParticle
+
+final case class Circuit(layers: Seq[Layer]) extends NanoParticle
