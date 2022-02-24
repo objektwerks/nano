5c962df052f65128feb66bf3fe108707b13c90ec
objektwerks <objektwerks@runbox.com>
2022-02-24 15:13:14 -0500

    simplified mac test

--- src/test/scala/nano/MACTest.scala
+++ src/test/scala/nano/MACTest.scala
@@ -7,23 +7,17 @@ class MACTest extends AnyFunSuite with Matchers:
   private val address = "50ed3c45f4ba"
 
   test("valid mac") {
-    val mac = MAC(address)
-    mac.isRight shouldBe true
-    mac.foreach(a => a shouldBe address)
+    MAC(address).foreach(a => a shouldBe address)
   }
 
   test("invalid mac") {
-    val mac = MAC("1a2b3c")
-    mac.isLeft shouldBe true
-    mac.foreach(a => a shouldBe "")
+    MAC("1a2b3c").foreach(a => a shouldBe "")
   }
 
   test("display mac") {
-    val mac = MAC(address)
-    mac.foreach(a => a.display shouldBe "50:ed:3c:45:f4:ba")
+    MAC(address).foreach(a => a.display shouldBe "50:ed:3c:45:f4:ba")
   }
 
   test("number mac") {
-    val mac = MAC(address)
-    mac.foreach(a => a.number shouldBe 876)
+    MAC(address).foreach(a => a.number shouldBe 876)
   }
