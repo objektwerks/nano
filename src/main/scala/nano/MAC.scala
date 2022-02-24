d0f9dcfd78f3625bcac4e45669555b47dde33c4a
objektwerks <objektwerks@runbox.com>
2022-02-24 14:54:33 -0500

    added number method to mac

--- src/main/scala/nano/MAC.scala
+++ src/main/scala/nano/MAC.scala
@@ -1,5 +1,7 @@
 package nano
 
+import java.lang.Integer
+
 import scala.language.strictEquality
 import scala.collection.mutable
 
@@ -29,3 +31,13 @@ extension (mac: MAC)
     builder += chars(8) += chars(9) += colon
     builder += chars(10) += chars(11)
     builder.toString
+  def number: Long =
+    val chars = mac.toArray
+    val builder = mutable.ArrayBuffer[Long]()
+    builder += Integer.parseInt( s"${chars(0)}${chars(1)}", 16)
+    builder += Integer.parseInt( s"${chars(2)}${chars(3)}", 16)
+    builder += Integer.parseInt( s"${chars(4)}${chars(5)}", 16)
+    builder += Integer.parseInt( s"${chars(6)}${chars(7)}", 16)
+    builder += Integer.parseInt( s"${chars(8)}${chars(9)}", 16)
+    builder += Integer.parseInt( s"${chars(10)}${chars(11)}", 16)
+    builder.sum
