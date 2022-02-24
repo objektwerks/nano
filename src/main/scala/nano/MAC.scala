942a58ec25f6b48c0a6e5b507fbc80b1fdfebc2b
objektwerks <objektwerks@runbox.com>
2022-02-24 07:32:54 -0500

    enhanced mac

--- src/main/scala/nano/MAC.scala
+++ src/main/scala/nano/MAC.scala
@@ -3,9 +3,15 @@ package nano
 import scala.language.strictEquality
 
 opaque type MAC = String
+type MACError = String
 
 object MAC:
-  def apply(mac: String): MAC = mac
+  def apply(mac: String): Either[MACError, MAC] =
+    Either.cond(
+      mac.length == 12,
+      mac,
+      s"MAC length must be 12, not ${mac.length}."
+    )
 
 given CanEqual[MAC, MAC] = CanEqual.derived
 
