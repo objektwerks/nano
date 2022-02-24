f6bce3341b4f5a18d9ae974cd0ae87a82a769922
objektwerks <objektwerks@runbox.com>
2022-02-24 07:41:57 -0500

    enhanced valid mac test

--- src/test/scala/nano/MACTest.scala
+++ src/test/scala/nano/MACTest.scala
@@ -5,8 +5,10 @@ import org.scalatest.matchers.should.Matchers
 
 class MACTest extends AnyFunSuite with Matchers:
   test("valid mac") {
-    val mac = MAC("1a2b3c4d5e6f")
+    val address = "1a2b3c4d5e6f"
+    val mac = MAC(address)
     mac.isRight shouldBe true
+    mac.foreach(a => a shouldBe address)
   }
 
   test("invalid mac") {
